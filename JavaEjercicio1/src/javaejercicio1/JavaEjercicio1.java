e270048dcc69d58eb1f11c53f5c31e70c0f4479b
LEONARDO ARIZA CAMARGO <103968269+leonardo-ariza@users.noreply.github.com>
2023-07-16 23:23:06 -0500

    Add files via upload

--- JavaEjercicio1/src/javaejercicio1/JavaEjercicio1.java
+++ JavaEjercicio1/src/javaejercicio1/JavaEjercicio1.java
@@ -6,43 +6,46 @@
 package javaejercicio1;
 import javax.swing.JOptionPane;  
 
-/**
- *
- * @author Aprendiz
- */
 public class JavaEjercicio1 {
 
-String nombre, genero, nacionalidad;
-int edad;
+    static String nombre, genero, nacionalidad;
+    static int edad;
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
-      nombre = Char.parschar(JOptionPane.showInputDialog(null, "Ingrese su nombre: "));
-      genero =   Char.parschar(JOptionPane.showInputDialog(null, "Ingrese su género: "));
-       edad = Integer.parseint(JoptionsPane.showInputDialog(null, "Ingrese su edad: "));
-      nacionalidad = Char.parschar(JOptionPane.showInputDialog(null, "Ingrese su nacionalidad: "));
-      
-      if (edad>=17) {
-        JoptionsPane.showMessageDialog(null, "La persona es mayor de edad");
-        else {
-        JoptionsPane.showMessageDialog(null, "La persona es menor de edad");
-                }
-      
-     if (genero==Hombre||genero==HOMBRE||genero==hombre||genero==Masculino||genero==MASCULINO||genero==masculino) {
-        JoptionsPane.showMessageDialog(null, "La persona es un hombre");
-        else {
-        JoptionsPane.showMessageDialog(null, "La persona es una mujer");
-                }
-     
-      
-      
-      
-     System.out.println("Número ingresado: " + numero); 
-    }
-    
-}
-    
-}
+        nombre = JOptionPane.showInputDialog(null, "Ingrese su nombre: ");
+        genero = JOptionPane.showInputDialog(null, "Ingrese su género: ");
+        edad = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese su edad: "));
+        nacionalidad = JOptionPane.showInputDialog(null, "Ingrese su nacionalidad: ");
+        
+        JOptionPane.showMessageDialog(null, "Su nombre es: "+ nombre);
+        
+        if (genero.equalsIgnoreCase("Hombre") || genero.equalsIgnoreCase("Masculino")) {
+            JOptionPane.showMessageDialog(null, "La persona es un hombre");
+        } else {
+            JOptionPane.showMessageDialog(null, "La persona es una mujer");
+        }
+        
+        if (edad > 17) {
+            JOptionPane.showMessageDialog(null, "La persona es mayor de edad, tiene "+ edad );
+        } else {
+            JOptionPane.showMessageDialog(null, "La persona es menor de edad, tiene: "+ edad );
+        }
+        
+        if (nacionalidad.equalsIgnoreCase("Colombiana")) {
+            JOptionPane.showMessageDialog(null, "La persona es de nacionalidad Colombiana");
+        } else {
+            JOptionPane.showMessageDialog(null, "La persona es un extranjero/extranjera");
+        }
+        
+        if (genero.equalsIgnoreCase("Hombre")|| genero.equalsIgnoreCase("Masculino") && edad > 17 && nacionalidad.equalsIgnoreCase("Colombiana")) {
+            
+            JOptionPane.showMessageDialog(null, nombre + " es apto para prestar el servicio militar");
+        }
+            else {
+            JOptionPane.showMessageDialog(null, nombre + " NO es apto para ingresar al servicio militar");
+        }
+            
+        }
+    }  
+
