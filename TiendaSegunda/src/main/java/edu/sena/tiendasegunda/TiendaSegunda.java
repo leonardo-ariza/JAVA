7f81988d79cc4e82562dec0120bc6c94ba61306b
LEONARDO ARIZA CAMARGO <pointerariza@gmail.com>
2023-08-23 06:41:01 -0500

    cambios tienda segunda

--- TiendaSegunda/src/main/java/edu/sena/tiendasegunda/TiendaSegunda.java
+++ TiendaSegunda/src/main/java/edu/sena/tiendasegunda/TiendaSegunda.java
@@ -1,19 +1,13 @@
- /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package edu.sena.tiendasegunda;
+
 import java.util.Scanner;
-
-
 
 public class TiendaSegunda {
 
     public static void main(String[] args) {
-        String persona=null;
+        String persona, persona2 = null;
         int opcion;
-        Scanner leer = new Scanner (System.in);
+        Scanner leer = new Scanner(System.in);
         System.out.print("Escoja una opcion de menu: \n");
         System.out.print("1) Client frecuente \n");
         System.out.print("2) Cliente corporativo \n");
@@ -21,42 +15,48 @@ public class TiendaSegunda {
         System.out.print("4) Cliente adulto mayor \n");
         System.out.print("5) Cliente primera infancia \n");
         opcion = leer.nextInt();
-        switch (opcion){
-            // && (productos>10)){
-            case 1:
-                System.out.println("¿El tipo de persona es natural?: ");
-                persona=(leer.nextLine());
-                
-                //System.out.println("Ingrese la cantidad de  productos: ");
-                //int productos = leer.nextInt();
-                if(persona == "natural"){ 
-                    
-                     System.out.print("Cliente frecuente aplica para descuento \n");
-                }
-                    else{
-                            
-                    System.out.print("Cliente frecuente NO aplica para descuento \n");        
-                            }
-                
-                break;
-                
-            case 2:
-                
-                break;
-            case 3:
-                
-                break;
-            case 4:
-                
-                break;
-            case 5:
-                
-                break;
-            default:
-            }
-            
-            
-        }
-    
-}
 
+        // Consume the newline character left by leer.nextInt()
+        leer.nextLine();
+
+        // Ask for the type of person (natural or not)
+        System.out.println("¿El tipo de persona es natural o corportativo?: ");
+        persona = leer.nextLine();
+        System.out.println("¿Es la persona hombre, mujer, niño o anciano: ");
+        persona2 = leer.nextLine();
+
+        switch (opcion) {
+            case 1:
+                if (persona.equalsIgnoreCase("natural") || persona.equalsIgnoreCase("si")) {
+                    System.out.print("Cliente frecuente aplica para descuento \n");
+                } else {
+                    System.out.print("Cliente frecuente NO aplica para descuento \n");
+                }
+                break;
+
+            case 2:
+                // Handle Cliente corporativo
+                break;
+
+            case 3:
+                // Handle Cliente madre lactante
+                break;
+
+            case 4:
+                // Handle Cliente adulto mayor
+                if (persona2.equalsIgnoreCase("anciano") || persona.equalsIgnoreCase("si")) {
+                    System.out.print("Cliente frecuente aplica para descuento \n");
+                } else {
+                    System.out.print("Cliente frecuente NO aplica para descuento \n");
+                }
+                break;
+
+            case 5:
+                // Handle Cliente primera infancia
+                break;
+
+            default:
+                System.out.println("Opción inválida.");
+        }
+    }
+}
